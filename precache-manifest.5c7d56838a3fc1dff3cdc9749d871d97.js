8995c972c5db271ac57ba35d19165d4c59756575
devleni <50588358+devleni@users.noreply.github.com>
2020-02-02 22:05:58 +0100

    Updates

---
+++ precache-manifest.5c7d56838a3fc1dff3cdc9749d871d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb3312066d1e5f8ad5d4921a4835b20",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "67e8062a9333f5cb6329",
+    "url": "/react-tictactoe/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "b95fae10107f17687d8d",
+    "url": "/react-tictactoe/static/js/2.fb03bbdc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tictactoe/static/js/2.fb03bbdc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67e8062a9333f5cb6329",
+    "url": "/react-tictactoe/static/js/main.ff939376.chunk.js"
+  },
+  {
+    "revision": "64275ddee66d13ce3342",
+    "url": "/react-tictactoe/static/js/runtime-main.f1dc3530.js"
+  }
+]);
